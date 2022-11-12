8bf5a0023d2008187ee7e49b9ebd0f1c65bc4b99
jazz-harris <mh216347@gmail.com>
2022-11-11 23:05:49 -0500

    Added another portfolio project

--- pages/components/SDPortfolio.jsx
+++ pages/components/SDPortfolio.jsx
@@ -76,9 +76,9 @@ const SDPortfolio = () => {
           </div>
           <div class="face face2">
             <div class="content ">
-              <p>Dynamic To-Do App with Quote API</p>
+              <p>Dynamic To-Do App with Quote API and CRUD</p>
               <p class="text-gray-500">
-                <small>FIREBASE API REACTJS</small>
+                <small>FIREBASE fetch API REACTJS</small>
               </p>
               <button></button>
               <div class="justify-center ">
@@ -129,21 +129,21 @@ const SDPortfolio = () => {
         <div class="card">
           <div class="face face1 ">
             <div class="content  ">
-            <img src="comingsoon.jpg" />
+            <img src="ChatAppLogo.png" />
             </div>
           </div>
           <div class="face face2">
             <div class="content ">
-              <p>Webscraping RealEstate data with Python and BeautifulSoup</p>
+              <p>Chat Application using FireBasev9 and React  </p>
               <p class="text-gray-500">
-                <small>Python BeautifulSoup</small>
+                <small>ReactJS FireBase HTML </small>
               </p>
               <button></button>
               <div class="justify-center ">
                 <div>
                   <a
                     className=" cursor-pointer "
-                    href="https://github.com/Jazz-Harris/WebScrapingRealEstateData"
+                    href="https://github.com/Jazz-Harris/ChatApp"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -158,6 +158,24 @@ const SDPortfolio = () => {
                     </button>
                     
                   </a>
+                  <div>
+                    <a
+                    className=" cursor-pointer "
+                    href="https://todoapp-jazzharris.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                    >
+                      <button
+                        type="button"
+                        class=" px-2  pb-2 pt-2.5 hover:animate-pulse bg-cyan-400 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
+                      >
+                        <span class="w-3 mr-2 mt-1">
+                        <img src="https://chata-27aa7.web.app/"/>
+                        </span>
+                        LIVE Version
+                      </button>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
