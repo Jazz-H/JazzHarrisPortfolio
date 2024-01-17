a66f862781da4deefc370bf36d9d3db1100de4f5
Jazz <mharris42@elon.edu>
2024-01-17 13:53:33 -0500

    Updated section

--- pages/components/DAPortfolio.jsx
+++ pages/components/DAPortfolio.jsx
@@ -47,44 +47,7 @@ const DAPortfolio = () => {
           </div>
        
 
-        <div class="floatcontainer">
-          <div class="card">
-            <div class="face face1 ">
-              <div class="content  ">
-                <img src="./assets/Covid19VisLogo.jpg" />
-              </div>
-            </div>
-            <div class="face face2">
-              <div class="content ">
-                <p>COVID19 Data Visualization Using Python</p>
-                <p class="text-gray-500">
-                  <small>Python Pandas Numpy Plotly Matplotlib</small>
-                </p>
-                <button></button>
-                <div class="justify-center ">
-                  <div>
-                    <a
-                      className=" cursor-pointer "
-                      href="https://github.com/Jazz-Harris/COVID19DataVIS/blob/main/COVID19DataVis.ipynb"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <button
-                        type="button"
-                        class=" px-2  pb-2 pt-2.5 mr-2 hover:animate-pulse bg-cyan-400 float-left text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out flex align-center"
-                      >
-                        <span class="w-3 mr-2 mt-1">
-                          <BsGithub />
-                        </span>
-                        Github
-                      </button>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-         </div>
+       
         </div>
         <div class="floatcontainer">
           <div class="card">
