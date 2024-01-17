f236e98ac18d18edca55bf5888286e984558a5b9
Jazz <mharris42@elon.edu>
2024-01-17 13:48:30 -0500

    Moving Sections 1

--- pages/components/FeaturedProjects.jsx
+++ pages/components/FeaturedProjects.jsx
@@ -7,65 +7,7 @@ const FeaturedProjects = () => {
         
     <div class="containerv2">
  
-      <div class="floatcontainer">
-      <div class="card">
-            <div class="face face1 ">
-              <div class="content  ">
-                <img src="./assets/ToDoAppLogo.png" />
-              </div>
-            </div>
-            <div class="face face2">
-              <div class="content ">
-                <p>To-Do App with Quote API</p>
-                <p class="text-gray-500">
-                  <small>
-                    Front End: React, React Hooks. Back End: Axios, REST API,
-                    built using CRUD and Firebase
-                  </small>
-                </p>
-                <button></button>
-                <div class="justify-center ">
-                  <div>
-                    <a
-                      className=" cursor-pointer "
-                      href="https://github.com/Jazz-Harris/To-DoApp"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <button
-                        type="button"
-                        class=" px-2  pb-2 pt-2.5 mr-2 hover:animate-pulse bg-cyan-400 float-left text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out flex align-center"
-                      >
-                        <span class="w-3 mr-2 mt-1">
-                          <BsGithub />
-                        </span>
-                        Github
-                      </button>
-                    </a>
-
-                    <div>
-                      <a
-                        className=" cursor-pointer "
-                        href="https://to-do-app-nu-cyan.vercel.app/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <button
-                          type="button"
-                          class=" px-2  pb-2 pt-2.5 hover:animate-pulse bg-cyan-400 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
-                        >
-                          <span class="w-3 mr-2 mt-1">
-                            <img src="https://img.icons8.com/cotton/64/null/hyperlink.png" />
-                          </span>
-                          LIVE Version
-                        </button>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              </div>
-              </div>
+     
         
      
 
@@ -217,7 +159,7 @@ const FeaturedProjects = () => {
         </div>
       </div>
       </div>
-    </div>
+ 
   </section>
   );
 };
