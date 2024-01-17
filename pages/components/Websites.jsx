80778633b71c706f62402441310a239b71fb16e5
Jazz <mharris42@elon.edu>
2024-01-17 15:01:16 -0500

    header

---
+++ pages/components/Websites.jsx
@@ -0,0 +1,203 @@
+import { BsGithub } from "react-icons/bs";
+import { SiTableau } from "react-icons/si";
+
+const SDPortfolio = () => {
+  return (
+    <section class="bodyv2">
+      <div class="containerv2">
+      <div class="floatcontainer"></div>
+      <div class="card">
+            <div class="face face1 ">
+              <div class="content  ">
+                <img src="./assets/ToDoAppLogo.png" />
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content ">
+                <p>To-Do App with Quote API</p>
+                <p class="text-gray-500">
+                  <small>
+                    Front End: React, React Hooks. Back End: Axios, REST API,
+                    built using CRUD and Firebase
+                  </small>
+                </p>
+                <button></button>
+                <div class="justify-center ">
+                  <div>
+                    <a
+                      className=" cursor-pointer "
+                      href="https://github.com/Jazz-Harris/To-DoApp"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <button
+                        type="button"
+                        class=" px-2  pb-2 pt-2.5 mr-2 hover:animate-pulse bg-cyan-400 float-left text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out flex align-center"
+                      >
+                        <span class="w-3 mr-2 mt-1">
+                          <BsGithub />
+                        </span>
+                        Github
+                      </button>
+                    </a>
+
+                    <div>
+                      <a
+                        className=" cursor-pointer "
+                        href="https://to-do-app-nu-cyan.vercel.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <button
+                          type="button"
+                          class=" px-2  pb-2 pt-2.5 hover:animate-pulse bg-cyan-400 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
+                        >
+                          <span class="w-3 mr-2 mt-1">
+                            <img src="https://img.icons8.com/cotton/64/null/hyperlink.png" />
+                          </span>
+                          LIVE Version
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+              </div>
+        
+        <div class="floatcontainer">
+        
+          <div class="card">
+            <div class="face face1 ">
+              <div class="content  ">
+                <img src="./assets/weatherappbg.png" className="object-cover" />
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content ">
+                <p>Weather Application</p>
+                <p class="text-gray-500 p-10">
+                  <small>
+                    Front End:React, TailWind CSS. Back END: Axios, REST API to
+                    use the OpenWeatherAPI. Other Frame works used: Nextjs.{" "}
+                  </small>
+                </p>
+                <button></button>
+                <div class="justify-center ">
+                  <div>
+                    <a
+                      className=" cursor-pointer "
+                      href="https://github.com/Jazz-Harris/WeatherApp"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <button
+                        type="button"
+                        class=" px-2  pb-2 pt-2.5 mr-2 hover:animate-pulse bg-cyan-400 float-left text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out flex align-center"
+                      >
+                        <span class="w-3 mr-2 mt-1">
+                          <BsGithub />
+                        </span>
+                        Github
+                      </button>
+                    </a>
+
+                    <div>
+                      <a
+                        className=" cursor-pointer "
+                        href="https://weather-app-sand-six-26.vercel.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <button
+                          type="button"
+                          class=" px-2  pb-2 pt-2.5 hover:animate-pulse bg-cyan-400 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
+                        >
+                          <span class="w-3 mr-2 mt-1">
+                            <img src="https://img.icons8.com/cotton/64/null/hyperlink.png" />
+                          </span>
+                          LIVE Version
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+
+        <div class="floatcontainer">
+      
+        
+         
+
+          <div class="card">
+            <div class="face face1 ">
+              <div class="content  ">
+                <img src="./assets/ChatAppLogo.png" />
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content ">
+                <p>Chat Application using Firebasev9 and React </p>
+                <p class="text-gray-500">
+                  <small>
+                    Front END: React, TailWind CSS, HTML. Other tools used
+                    Firebase
+                  </small>
+                </p>
+                <button></button>
+                <div class="justify-center ">
+                  <div>
+                    <a
+                      className=" cursor-pointer "
+                      href="https://github.com/Jazz-Harris/ChatApp"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <button
+                        type="button"
+                        class=" px-2  pb-2 pt-2.5 mr-2 hover:animate-pulse bg-cyan-400 float-left text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out flex align-center"
+                      >
+                        <span class="w-3 mr-2 mt-1">
+                          <BsGithub />
+                        </span>
+                        Github
+                      </button>
+                    </a>
+                    <div>
+                      <a
+                        className=" cursor-pointer "
+                        href="https://chata-27aa7.web.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <button
+                          type="button"
+                          class=" px-2  pb-2 pt-2.5 hover:animate-pulse bg-cyan-400 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-cyan-600 hover:shadow-lg focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
+                        >
+                          <span class="w-3 mr-2 mt-1">
+                            <img src="https://img.icons8.com/cotton/64/null/hyperlink.png" />
+                          </span>
+                          LIVE Version
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+      
+
+        </div>
+      </div>
+
+      
+    </section>
+  );
+};
+export default SDPortfolio;
