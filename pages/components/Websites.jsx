eb3490eb532205842462e484add8cc557775bf0b
Jazz <mharris42@elon.edu>
2024-01-17 16:23:41 -0500

    Title

--- pages/components/Websites.jsx
+++ pages/components/Websites.jsx
@@ -6,14 +6,17 @@ const Websites = () => {
 
     
     <section class="bodyv2">
-      <div class="containerv2">
-      <div class="floatcontainer"></div>
 
-      <section>
+<section>
       <h4 className=" text-center text-base md:text-3xl   font-semibold dark: text-black dark:text-white">
         <bold>Websites</bold>{" "}
       </h4>
       </section>
+      <div class="containerv2">
+      <div class="floatcontainer"></div>
+
+     
+
       <div class="card">
             <div class="face face1 ">
               <div class="content  ">
