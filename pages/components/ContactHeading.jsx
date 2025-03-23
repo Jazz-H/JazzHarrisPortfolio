b44a47d13f5ddf6adfadd67271b13b6fb8e408a0
Jazz Harris <mh216347@gmail.com>
2025-03-23 18:41:45 +0000

    testing section update

--- pages/components/ContactHeading.jsx
+++ pages/components/ContactHeading.jsx
@@ -7,6 +7,9 @@ const ContactHeading = () => {
         <div id = "Contact"></div>
       <div class="flex min-w-screen items-center text-center pt-20 lg:pt-35 lg:pr-5  dark:bg-gray-900 ">
         <div    class="mx-auto w-full max-w-max ">
+          
+          
+          <section class = " mx-12 pb-4 px-5 mb-20 3xl:p-10 text-sm shadow-md rounded-lg dark:shadow-grey lg:mt-10 lg:mx-20   lg:p-5 xl:mx-20  text-black dark:text-black dark:bg-white dark:shadow-white flex items-center justify-center">
           <h3 class="my-3 text-4xl sm:text-5xl md:text-5xl mb-8 xl:text-8xl xl:mb-4 font-semibold text-black dark:text-white">
             Wanna connect?
           </h3>
@@ -18,12 +21,7 @@ const ContactHeading = () => {
             I'm always looking for new opportunities and the prospect to create
             something great!
           </p>
-          
-          <section class = " mx-12 pb-4 px-5 mb-20 3xl:p-10 text-sm shadow-md rounded-lg dark:shadow-grey lg:mt-10 lg:mx-20   lg:p-5 xl:mx-20  text-black dark:text-black dark:bg-white dark:shadow-white flex items-center justify-center">
-   
-          <ContactForm
-     
-          ></ContactForm>
+        
      
       </section>
         </div>
