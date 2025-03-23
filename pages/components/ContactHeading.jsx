7dd03644dc34e51d2ee60dae0c1ed9c58af13f13
Jazz Harris <mh216347@gmail.com>
2025-03-23 18:36:36 +0000

    updated contact form

--- pages/components/ContactHeading.jsx
+++ pages/components/ContactHeading.jsx
@@ -12,14 +12,20 @@ const ContactHeading = () => {
           </h3>
           <p class="text-md mt-2 md:text-2xl lg:px-5 text-gray-400 xl:text-2xl xl:mt-10 dark:text-gray-200">
             Email me at <span class="text-cyan-400">mauraharris948@gmail.com</span> or
-            contact me on linkedin.
+            use the form below to send me a message.
           </p>
           <p class=" text-xs p-5 md:text-lg md:px-5 text-gray-400 xl:text-1xl dark:text-gray-200">
             I'm always looking for new opportunities and the prospect to create
             something great!
           </p>
           
-          
+          <section class = " mx-12 pb-4 px-5 mb-20 3xl:p-10 text-sm shadow-md rounded-lg dark:shadow-grey lg:mt-10 lg:mx-20   lg:p-5 xl:mx-20  text-black dark:text-black dark:bg-white dark:shadow-white flex items-center justify-center">
+   
+          <ContactForm
+     
+          ></ContactForm>
+     
+      </section>
         </div>
       </div>
     
