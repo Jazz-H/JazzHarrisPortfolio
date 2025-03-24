8472ab5884033bbfb073a7966fb4e7d51a0dd796
Jazz Harris <mh216347@gmail.com>
2025-03-24 12:15:35 +0000

    Updated Header spelling

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div class=" p-10 xs:mb-40 dark:bg-gray-900">
         <h3 className="text-1xl mt-40 pb-10 lowercase text-left  xs:text-xl  sm:px-5 sm:pt-10 md:text-2xl lg:text-3xl 3xl:ml-20 3xl:text-5xl 3xl:mt-40 text-gray-800 font-medium dark:text-gray-300 2xl:mb-20">
-          Hi, My name is
+          Hi, my name is
         </h3>
         <div className=" pt-5  md:mt-10 text-center md:text-center  md:pl-0 g:text-center">
           <h1 className="text-3xl   uppercase font-black mb-8 xs:text-4xl sm:text-5xl  md:text-8xl  xl:text-9xl text-cyan-400 2xl:mb-16  drop-shadow-xl shadow-black dark:drop-shadow-xl ">
